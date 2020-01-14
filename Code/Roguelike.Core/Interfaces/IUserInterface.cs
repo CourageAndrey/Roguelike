584841fee1749b186c6ca093cc83d3117c2c6e1c
Andrei Bychko <andrei.a.bychko@gmail.com>
2020-01-14 22:29:19 +0300

    Add not implemented possibilities to chat, trade and pickpocket.

--- Code/Roguelike.Core/Interfaces/IUserInterface.cs
+++ Code/Roguelike.Core/Interfaces/IUserInterface.cs
@@ -11,5 +11,8 @@ namespace Roguelike.Core.Interfaces
 		bool TrySelectItem(Game game, string question, IEnumerable<ListItem> items, out ListItem selectedItem);
 		bool TrySelectItems(Game game, string question, IEnumerable<ListItem> items, out IList<ListItem> selectedItems);
 		void ShowCharacter(Game game, Humanoid humanoid);
+		ActionResult BeginChat(Game game, Humanoid humanoid);
+		ActionResult BeginTrade(Game game, Humanoid humanoid);
+		ActionResult BeginPickpocket(Game game, Humanoid humanoid);
 	}
 }

1fbb55625c7b7e0477eb552248d517c48c4ac09b
Andrei Bychko <courage.andrey@gmail.com>
2021-01-01 10:15:14 +0300

    Add IInterlocutor interface.

---
+++ Code/Roguelike.Core/Interfaces/IInterlocutor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+using Roguelike.Core.ActiveObjects;
+using Roguelike.Core.Chat;
+
+namespace Roguelike.Core.Interfaces
+{
+	public interface IInterlocutor
+	{
+		string GetName(Humanoid interlocutor);
+
+		Attitude GetAttitude(Humanoid interlocutor);
+
+		ICollection<Topic> GetTopics(Humanoid interlocutor);
+
+		Text Discuss(Humanoid interlocutor, Topic topic);
+	}
+}

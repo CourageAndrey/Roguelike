ecde52992f3aa278ba329256cb2b0d17e04ea997
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-08-15 17:13:30 +0300

    Implement all buttons within PersonWindow.

--- Code/Roguelike.WpfClient/Windows/CharacherWindow.xaml.cs
+++ Code/Roguelike.WpfClient/Windows/CharacherWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows.Input;
+﻿using System.Windows;
+using System.Windows.Input;
 
+using Roguelike.Core.ActiveObjects;
 using Roguelike.Core.Localization;
 
 namespace Roguelike.WpfClient
@@ -17,12 +19,85 @@ namespace Roguelike.WpfClient
 			set { buttonsPanel.DataContext = value; }
 		}
 
+		public Humanoid Character
+		{ get; set; }
+
 		private void exitKeyDown(object sender, KeyEventArgs e)
 		{
 			if (e.Key == Key.Escape)
 			{
 				DialogResult = false;
 			}
+		}
+
+		private void generalClick(object sender, RoutedEventArgs e)
+		{
+			var dialog = new Windows.PersonWindow
+			{
+				GameLanguage = GameLanguage,
+				Character = Character,
+			};
+			dialog.ShowDialog();
+		}
+
+		private void bodyClick(object sender, RoutedEventArgs e)
+		{
+			var dialog = new Windows.BodyWindow
+			{
+				GameLanguage = GameLanguage,
+				Character = Character,
+			};
+			dialog.ShowDialog();
+		}
+
+		private void effectsClick(object sender, RoutedEventArgs e)
+		{
+			var dialog = new Windows.EffectsWindow
+			{
+				GameLanguage = GameLanguage,
+				Character = Character,
+			};
+			dialog.ShowDialog();
+		}
+
+		private void statsClick(object sender, RoutedEventArgs e)
+		{
+			var dialog = new Windows.StatsWindow
+			{
+				GameLanguage = GameLanguage,
+				Character = Character,
+			};
+			dialog.ShowDialog();
+		}
+
+		private void skillsClick(object sender, RoutedEventArgs e)
+		{
+			var dialog = new Windows.SkillsWindow
+			{
+				GameLanguage = GameLanguage,
+				Character = Character,
+			};
+			dialog.ShowDialog();
+		}
+
+		private void wearedClick(object sender, RoutedEventArgs e)
+		{
+			var dialog = new Windows.WearingsWindow
+			{
+				GameLanguage = GameLanguage,
+				Character = Character,
+			};
+			dialog.ShowDialog();
+		}
+
+		private void inventoryClick(object sender, RoutedEventArgs e)
+		{
+			var dialog = new Windows.InventoryWindow
+			{
+				GameLanguage = GameLanguage,
+				Character = Character,
+			};
+			dialog.ShowDialog();
 		}
 	}
 }

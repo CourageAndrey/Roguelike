7dd99a1a6508dcc31d05a89a9bcce2115dd26997
Andrei Bychko <andrei.a.bychko@gmail.com>
2021-08-15 18:57:57 +0300

    Detail weared items interfaces and update IManequin.

--- Code/Roguelike.Core/Interfaces/IItem.cs
+++ Code/Roguelike.Core/Interfaces/IItem.cs
@@ -5,4 +5,32 @@
 		ItemType Type
 		{ get; }
 	}
+
+	#region Item interfaces
+
+	public interface IHeadWear : IItem
+	{ }
+
+	public interface IUpperBodyWear : IItem
+	{ }
+
+	public interface ILowerBodyWear : IItem
+	{ }
+
+	public interface ICoverWear : IItem
+	{ }
+
+	public interface IHandWear : IItem
+	{ }
+
+	public interface IFootWear : IItem
+	{ }
+
+	public interface INecklace : IItem
+	{ }
+
+	public interface IRing : IItem
+	{ }
+
+	#endregion
 }

6dac7d4b61abd472f263016016400948d4a68d17
Andrei Bychko <courage.andrey@gmail.com>
2021-08-15 19:16:50 +0300

    Extract class WorldGenerator.

---
+++ Code/Roguelike.Core/WorldGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Roguelike.Core
+{
+	public static class WorldGenerator
+	{
+		public static IReadOnlyCollection<Region> GenerateRegions(this World world, int count)
+		{
+			var regions = new Region[count];
+			for (int r = 0; r < count; r++)
+			{
+				regions[r] = new Region(world);
+			}
+			return new ReadOnlyCollection<Region>(regions);
+		}
+	}
+}

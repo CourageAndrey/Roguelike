36074ee4edc8d72cd4439b216d1d68e4cff4fb06
Andrei Bychko <courage.andrey@gmail.com>
2021-08-15 17:13:30 +0300

    Implement all buttons within PersonWindow.

---
+++ Code/Roguelike.WpfClient/Windows/WearingsWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+
+using Roguelike.Core.ActiveObjects;
+using Roguelike.Core.Localization;
+
+namespace Roguelike.WpfClient.Windows
+{
+	public partial class WearingsWindow
+	{
+		public WearingsWindow()
+		{
+			InitializeComponent();
+		}
+
+		public Language GameLanguage
+		{ get; set; }
+
+		public Humanoid Character
+		{ get; set; }
+
+		private void exitKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Escape)
+			{
+				DialogResult = false;
+			}
+		}
+	}
+}

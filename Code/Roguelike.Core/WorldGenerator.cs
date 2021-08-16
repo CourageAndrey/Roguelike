3e0a5fdae9581e6aa28e7ba849de6d384f5032e5
Andrei Bychko <courage.andrey@gmail.com>
2021-08-16 21:33:50 +0300

    Implement village generation.

--- Code/Roguelike.Core/WorldGenerator.cs
+++ Code/Roguelike.Core/WorldGenerator.cs
@@ -50,6 +50,18 @@ namespace Roguelike.Core
 
 			for (int x = x1; x <= x2; x++)
 			{
+				for (int y = y1; y <= y2; y++)
+				{
+					var removedObjects = new List<Object>(region.GetCell(x, y, z).Objects);
+					foreach (var o in removedObjects)
+					{
+						o.MoveTo(null);
+					}
+				}
+			}
+
+			for (int x = x1; x <= x2; x++)
+			{
 				new Wall().MoveTo(region.GetCell(x, y1, z));
 				new Wall().MoveTo(region.GetCell(x, y2, z));
 			}
@@ -72,19 +84,83 @@ namespace Roguelike.Core
 			new Door().MoveTo(doorCell);
 		}
 
-		public static void CreateVillage(this Region region, Balance balance, int x1, int x2, int y1, int y2, int z)
+		public static void CreateVillage(this Region region, Balance balance, Random seed, int x1, int x2, int y1, int y2, int z)
 		{
 			if (Math.Abs(x1 - x2) < 10 || Math.Abs(y1 - y2) < 10) throw new Exception("Village is too small - 10x10 is minimal size.");
 
-			region.CreateRoom(1, 5, 1, 5, 0, Direction.Up);
-			new Pool().MoveTo(region.GetCell(6, 7, 0));
-			new Tree().MoveTo(region.GetCell(7, 7, 0));
-			new Fire().MoveTo(region.GetCell(2, 2, 0));
-			new Bed().MoveTo(region.GetCell(4, 4, 0));
-			var npc = new Npc(true, Time.FromYears(balance, 50), new Properties(), new Inventory(), "John Smith");
-			npc.MoveTo(region.GetCell(2, 10, 0));
-			new Npc(true, Time.FromYears(balance, 14), new Properties(), new Inventory(), "Jack Smiley").Die("just die").MoveTo(region.GetCell(0, 1, 0));
-			new Animal(false, Time.FromYears(balance, 5), new Properties(), new Inventory()) { Owner = npc }.MoveTo(region.GetCell(3, 10, 0));
+			const int minHouseDimension = 5;
+			const int maxHouseDimension = 7;
+			var possibleDoorDirections = new List<Direction>
+			{
+				Direction.Down,
+				Direction.Up,
+				Direction.Left,
+				Direction.Right,
+			};
+
+			int totalHouses = 0;
+
+			int treesCount = (x2 - x1) * (y2 - y1) / 10;
+			for (int i = 0; i < treesCount; i++)
+			{
+				new Tree().placeIntoFreeCell(region, seed, x1, x2, y1, y2, z);
+			}
+
+			int poolsCount = (x2 - x1) * (y2 - y1) / 30;
+			for (int i = 0; i < poolsCount; i++)
+			{
+				new Pool().placeIntoFreeCell(region, seed, x1, x2, y1, y2, z);
+			}
+
+			int houseY1 = y1 + 1;
+			while (houseY1 + maxHouseDimension + 1 < y2)
+			{
+				int houseX1 = x1 + 1;
+				while (houseX1 + maxHouseDimension + 1 < x2)
+				{
+					int finalX1 = seed.Next(houseX1 - 1, houseX1 + 1);
+					int finalX2 = finalX1 + seed.Next(minHouseDimension, maxHouseDimension) - 1;
+					int finalY1 = seed.Next(houseY1 - 1, houseY1 + 1);
+					int finalY2 = finalY1 + seed.Next(minHouseDimension, maxHouseDimension) - 1;
+
+					var doorDirection = possibleDoorDirections[seed.Next(0, possibleDoorDirections.Count - 1)];
+
+					region.CreateRoom(finalX1, finalX2, finalY1, finalY2, z, doorDirection);
+					totalHouses++;
+
+					new Fire().placeIntoFreeCell(region, seed, finalX1 + 1, finalX2 - 1, finalY1 + 1, finalY2 - 1, z);
+					new Bed().placeIntoFreeCell(region, seed, finalX1 + 1, finalX2 - 1, finalY1 + 1, finalY2 - 1, z);
+					new Bed().placeIntoFreeCell(region, seed, finalX1 + 1, finalX2 - 1, finalY1 + 1, finalY2 - 1, z);
+
+					houseX1 += maxHouseDimension + seed.Next(1, 2);
+				}
+
+				houseY1 += maxHouseDimension + seed.Next(1, 2);
+			}
+
+			for (int i = 0; i < totalHouses; i++)
+			{
+				var husband = new Npc(true, Time.FromYears(balance, 50), new Properties(), new Inventory(), "John Smith " + i);
+				husband.placeIntoFreeCell(region, seed, x1, x2, y1, y2, z);
+
+				var wife = new Npc(false, Time.FromYears(balance, 50), new Properties(), new Inventory(), "Mary Poppins " + i);
+				wife.placeIntoFreeCell(region, seed, x1, x2, y1, y2, z);
+
+				var pet = new Animal(false, Time.FromYears(balance, 5), new Properties(), new Inventory()) { Owner = husband };
+				pet.placeIntoFreeCell(region, seed, x1, x2, y1, y2, z);
+			}
+		}
+
+		private static void placeIntoFreeCell(this Object @object, Region region, Random seed, int x1, int x2, int y1, int y2, int z)
+		{
+			Cell cell;
+			do
+			{
+				int x = seed.Next(x1, x2);
+				int y = seed.Next(y1, y2);
+				cell = region.GetCell(x, y, z);
+			} while (cell.Objects.Count > 0 && cell.Objects.All(o => !o.IsSolid));
+			@object.MoveTo(cell);
 		}
 
 		public static void MakeMapKnown(this Hero hero, int viewDistance)

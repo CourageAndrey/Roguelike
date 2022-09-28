5c19e52d4372eafc8350fc17c899b7700c0b94b9
Andrei Bychko <andrei.a.bychko@gmail.com>
2022-09-28 20:58:23 +0300

    Extract Manequin.ChangeEquipmentPosition method.

--- Code/Roguelike.Core/ActiveObjects/Manequin.cs
+++ Code/Roguelike.Core/ActiveObjects/Manequin.cs
@@ -14,67 +14,37 @@ namespace Roguelike.Core.ActiveObjects
 		public IHeadWear HeadWear
 		{
 			get { return _headWear; }
-			private set
-			{
-				if (value == null) throw new ArgumentNullException(nameof(value));
-				_headWear = value;
-				RaiseEquipmentChanged();
-			}
+			private set { ChangeEquipmentPosition(value, ref _headWear); }
 		}
 
 		public IUpperBodyWear UpperBodyWear
 		{
 			get { return _upperBodyWear; }
-			private set
-			{
-				if (value == null) throw new ArgumentNullException(nameof(value));
-				_upperBodyWear = value;
-				RaiseEquipmentChanged();
-			}
+			private set { ChangeEquipmentPosition(value, ref _upperBodyWear); }
 		}
 
 		public ILowerBodyWear LowerBodyWear
 		{
 			get { return _lowerBodyWear; }
-			private set
-			{
-				if (value == null) throw new ArgumentNullException(nameof(value));
-				_lowerBodyWear = value;
-				RaiseEquipmentChanged();
-			}
+			private set { ChangeEquipmentPosition(value, ref _lowerBodyWear); }
 		}
 
 		public ICoverWear CoverWear
 		{
 			get { return _coverWear; }
-			private set
-			{
-				if (value == null) throw new ArgumentNullException(nameof(value));
-				_coverWear = value;
-				RaiseEquipmentChanged();
-			}
+			private set { ChangeEquipmentPosition(value, ref _coverWear); }
 		}
 
 		public IHandWear HandsWear
 		{
 			get { return _handsWear; }
-			private set
-			{
-				if (value == null) throw new ArgumentNullException(nameof(value));
-				_handsWear = value;
-				RaiseEquipmentChanged();
-			}
+			private set { ChangeEquipmentPosition(value, ref _handsWear); }
 		}
 
 		public IFootWear FootsWear
 		{
 			get { return _footsWear; }
-			private set
-			{
-				if (value == null) throw new ArgumentNullException(nameof(value));
-				_footsWear = value;
-				RaiseEquipmentChanged();
-			}
+			private set { ChangeEquipmentPosition(value, ref _footsWear); }
 		}
 
 		public ICollection<IJewelry> Jewelry
@@ -127,6 +97,16 @@ namespace Roguelike.Core.ActiveObjects
 			jewelryCollection.ItemAdded += jewelryChanged;
 			jewelryCollection.ItemRemoved += jewelryChanged;
 			Jewelry = jewelryCollection;
+		}
+
+		private void ChangeEquipmentPosition<ItemT>(ItemT newItem, ref ItemT itemSlot)
+			where ItemT : IWear
+		{
+			if (newItem == null) throw new ArgumentNullException(nameof(newItem));
+
+			itemSlot = newItem;
+
+			RaiseEquipmentChanged();
 		}
 	}
 }

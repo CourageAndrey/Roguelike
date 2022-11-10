e1bb976d40b3d438457349be4663a95127f66851
Andrei Bychko <andrei.a.bychko@gmail.com>
2022-11-10 17:28:29 +0200

    Move IWear and IWeapon interfaces to separate files.

---
+++ Code/Roguelike.Core/Interfaces/IWeapon.cs
@@ -0,0 +1,16 @@
+﻿namespace Roguelike.Core.Interfaces
+{
+	public interface IWeapon : IItem
+	{
+		bool IsRange
+		{ get; }
+
+		event EventHandler<IWeapon, IAlive> PreparedForBattle;
+
+		event EventHandler<IWeapon, IAlive> StoppedBattle;
+
+		void RaisePreparedForBattle(IAlive who);
+
+		void RaiseStoppedBattle(IAlive who);
+	}
+}
---
+++ Code/Roguelike.Core/Interfaces/IWear.cs
@@ -0,0 +1,38 @@
+﻿namespace Roguelike.Core.Interfaces
+{
+	public interface IWear : IItem
+	{
+		event EventHandler<IWear, IAlive> Equipped;
+
+		event EventHandler<IWear, IAlive> Unequipped;
+
+		void RaiseEquipped(IAlive who);
+
+		void RaiseUnequipped(IAlive who);
+	}
+
+	#region Wear interfaces
+
+	public interface IHeadWear : IWear
+	{ }
+
+	public interface IUpperBodyWear : IWear
+	{ }
+
+	public interface ILowerBodyWear : IWear
+	{ }
+
+	public interface ICoverWear : IWear
+	{ }
+
+	public interface IHandWear : IWear
+	{ }
+
+	public interface IFootWear : IWear
+	{ }
+
+	public interface IJewelry : IWear
+	{ }
+
+	#endregion
+}

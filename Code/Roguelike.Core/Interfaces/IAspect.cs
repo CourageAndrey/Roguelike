6bfabd0fb36ad86821de39d6432e965139dbef8b
Andrei Bychko <courage.andrey@gmail.com>
2022-11-18 11:29:55 +0200

    Extract IAspect interface.

---
+++ Code/Roguelike.Core/Interfaces/IAspect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Roguelike.Core.Interfaces
+{
+	public interface IAspect
+	{ }
+
+	public static class AspectExtensions
+	{
+		public static bool Is<AspectT>(this IItem item)
+			where AspectT : IAspect
+		{
+			return item.Aspects.OfType<AspectT>().Any();
+		}
+
+		public static IEnumerable<IItem> Select<AspectT>(this IEnumerable<IItem> items)
+			where AspectT : IAspect
+		{
+			return items.Where(item => item.Is<AspectT>());
+		}
+
+		public static AspectT GetAspect<AspectT>(this IItem item)
+			where AspectT : IAspect
+		{
+			return item.Aspects.OfType<AspectT>().Single();
+		}
+	}
+}

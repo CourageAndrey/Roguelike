43fd4bac16b2144d452f2737b77a86c85ed77b38
Andrei Bychko <andrei.a.bychko@gmail.com>
2022-12-09 11:42:18 +0200

    Extract ILogSource interface.

---
+++ Code/Roguelike.Core/Interfaces/ILogSource.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Roguelike.Core.Interfaces
+{
+	public interface ILogSource
+	{
+		void WriteToLog(ICollection<string> messages);
+
+		event EventHandler<IObject, ICollection<string>> OnLogMessage;
+	}
+
+	public static class LogSourceExtensions
+	{
+		public static void WriteToLog(this ILogSource obj, string message)
+		{
+			obj.WriteToLog(new[] { message });
+		}
+	}
+}

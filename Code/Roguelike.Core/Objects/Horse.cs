d123761bb8a6501042d2be45fbb6effe11d8d0b7
Andrei Bychko <courage.andrey@gmail.com>
2022-12-16 11:48:42 +0200

    Implement horse's AI.

--- Code/Roguelike.Core/Objects/Horse.cs
+++ Code/Roguelike.Core/Objects/Horse.cs
@@ -5,6 +5,7 @@ using Roguelike.Core.ActiveObjects;
 using Roguelike.Core.Aspects;
 using Roguelike.Core.Configuration;
 using Roguelike.Core.Interfaces;
+using Roguelike.Core.Items;
 using Roguelike.Core.Localization;
 
 namespace Roguelike.Core.Objects
@@ -27,10 +28,63 @@ namespace Roguelike.Core.Objects
 
 		protected override ActionResult DoImplementation()
 		{
-#warning Horse is too passive.
 			var game = this.GetGame();
 			var balance = game.Balance;
-			return new ActionResult(Time.FromYears(balance.Time, 1), string.Empty);
+
+			if (State.IsThirsty)
+			{
+				var waterSource = CurrentCell.Objects.Select<IObject, WaterSource>().FirstOrDefault();
+				if (waterSource != null)
+				{
+					return waterSource.GetAspect<WaterSource>().Drink(this);
+				}
+				else
+				{
+					var waterCell = CurrentCell.FindClosiest(cell => cell.Objects.Any(o => o.Is<WaterSource>()), balance.Distance.AiRange);
+					waterSource = waterCell != null
+						? waterCell.Objects.Select<IObject, WaterSource>().First()
+						: null;
+
+					var nextStep = waterSource != null
+						? Ai.CalculateRoute(this.GetRegion(), this.GetPosition(), waterSource.CurrentCell.Position).Skip(1).FirstOrDefault()
+						: null;
+					if (nextStep != null)
+					{
+						return this.TryMove(this.GetPosition().GetDirection(nextStep));
+					}
+					else
+					{
+						return ActionResult.Wait(this);
+					}
+				}
+			}
+			else if (State.IsHungry)
+			{
+				if (CurrentCell.Background == CellBackground.Grass)
+				{
+					return Eat(ItemFactory.CreateGrass(State.GetFoodToFull()));
+				}
+				else
+				{
+					Cell grassCell = CurrentCell.FindClosiest(cell => cell.Background == CellBackground.Grass, balance.Distance.AiRange);
+
+					var nextStep = grassCell != null
+						? Ai.CalculateRoute(this.GetRegion(), this.GetPosition(), grassCell.Position).Skip(1).FirstOrDefault()
+						: null;
+					if (nextStep != null)
+					{
+						return this.TryMove(this.GetPosition().GetDirection(nextStep));
+					}
+					else
+					{
+						return ActionResult.Wait(this);
+					}
+				}
+			}
+			else
+			{
+				return new ActionResult(Time.FromTicks(balance.Time, 60 * 60 * 1000), string.Empty);
+			}
 		}
 
 		public override Body CreateBody()

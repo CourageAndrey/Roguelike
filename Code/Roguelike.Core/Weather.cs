0e8ca6f930a5dabc4cda7cb2bfb6cf3bcb320f5d
Andrei Bychko <courage.andrey@gmail.com>
2022-12-21 16:36:39 +0200

    Make time balance readable.

--- Code/Roguelike.Core/Weather.cs
+++ Code/Roguelike.Core/Weather.cs
@@ -50,7 +50,7 @@ namespace Roguelike.Core
 			Precipitation = false;
 
 			var seed = new Random(DateTime.Now.Millisecond);
-			var balance = Region.World.Game.Balance.Time;
+			var balance = Region.World.Time.Balance;
 			NextChangeTime = Region.World.Time.AddTicks(seed.Next((int)(balance.TicksInDay / 48), (int)(balance.TicksInDay * 3)));
 
 			Volatile.Read(ref Changed)?.Invoke(this, EventArgs.Empty);

85d9c02dc8a77b2c1e9947f921ee6c0415c2a3f2
Andrei Bychko <andrei.a.bychko@gmail.com>
2025-11-07 13:06:02 +0100

    Rearrange methods inside WorldGenerator.

--- Code/Roguelike.Core/WorldGenerator.cs
+++ Code/Roguelike.Core/WorldGenerator.cs
@@ -23,7 +23,84 @@ namespace Roguelike.Core
 			{
 				regions[r] = new Region(world, balance);
 			}
+
 			return new ReadOnlyCollection<Region>(regions);
+		}
+
+		public static void CreateVillage(this Region region, Balance balance, Random seed, Language language, int x1, int x2, int y1, int y2, int z)
+		{
+			if (Math.Abs(x1 - x2) < 10 || Math.Abs(y1 - y2) < 10) throw new Exception("Village is too small - 10x10 is minimal size.");
+
+			const int minHouseDimension = 5;
+			const int maxHouseDimension = 7;
+
+			int treesCount = (x2 - x1) * (y2 - y1) / 10;
+			for (int i = 0; i < treesCount; i++)
+			{
+				new Tree().placeIntoFreeCell(region, seed, x1, x2, y1, y2, z);
+			}
+
+			int poolsCount = (x2 - x1) * (y2 - y1) / 30;
+			for (int i = 0; i < poolsCount; i++)
+			{
+				new Pool().placeIntoFreeCell(region, seed, x1, x2, y1, y2, z);
+			}
+
+			var houses = new List<House>();
+			int houseY1 = y1 + 1;
+			while (houseY1 + maxHouseDimension + 1 < y2)
+			{
+				int houseX1 = x1 + 1;
+				while (houseX1 + maxHouseDimension + 1 < x2)
+				{
+					houses.Add(region.CreateHouse(seed, minHouseDimension, maxHouseDimension, houseX1, houseY1, z));
+
+					houseX1 += maxHouseDimension + seed.Next(1, 2);
+				}
+
+				houseY1 += maxHouseDimension + seed.Next(1, 2);
+			}
+
+			for (int i = 0; i < houses.Count; i++)
+			{
+				var race = Race.PlainsMan;
+				var profession = Profession.Everyman;
+				string surname = profession.IsSurname
+					? profession.GetName(language.Character.Professions)
+					: race.Surnames[i % race.Surnames.Count];
+				var husband = region.CreateFamily(balance, seed, race, profession, surname, x1, x2, y1, y2, z, houses[i]);
+
+				region.CreateAnimals(balance, seed, husband, x1, x2, y1, y2, z);
+			}
+		}
+
+		public static House CreateHouse(this Region region, Random seed, int minHouseDimension, int maxHouseDimension, int houseX1, int houseY1, int z)
+		{
+			int finalX1 = seed.Next(houseX1 - 1, houseX1 + 1);
+			int finalX2 = finalX1 + seed.Next(minHouseDimension, maxHouseDimension) - 1;
+			int finalY1 = seed.Next(houseY1 - 1, houseY1 + 1);
+			int finalY2 = finalY1 + seed.Next(minHouseDimension, maxHouseDimension) - 1;
+
+			var doorDirection = Directions.All4.GetRandom(seed);
+
+			region.CreateWalls(seed, finalX1, finalX2, finalY1, finalY2, z, doorDirection);
+
+			new Fire().placeIntoFreeCell(region, seed, finalX1 + 1, finalX2 - 1, finalY1 + 1, finalY2 - 1, z);
+			new Bed().placeIntoFreeCell(region, seed, finalX1 + 1, finalX2 - 1, finalY1 + 1, finalY2 - 1, z);
+			new Bed().placeIntoFreeCell(region, seed, finalX1 + 1, finalX2 - 1, finalY1 + 1, finalY2 - 1, z);
+
+			var allHouseCells = new List<Cell>();
+			for (int x = finalX1; x <= finalX2; x++)
+			{
+				for (int y = finalY1; y <= finalY2; y++)
+				{
+					allHouseCells.Add(region.GetCell(x, y, z));
+				}
+			}
+
+			var house = new House(allHouseCells);
+			region.Places.Add(house);
+			return house;
 		}
 
 		public static void CreateWalls(this Region region, Random seed, int x1, int x2, int y1, int y2, int z, Direction doorSide)
@@ -98,35 +175,6 @@ namespace Roguelike.Core
 			}
 		}
 
-		public static House CreateHouse(this Region region, Random seed, int minHouseDimension, int maxHouseDimension, int houseX1, int houseY1, int z)
-		{
-			int finalX1 = seed.Next(houseX1 - 1, houseX1 + 1);
-			int finalX2 = finalX1 + seed.Next(minHouseDimension, maxHouseDimension) - 1;
-			int finalY1 = seed.Next(houseY1 - 1, houseY1 + 1);
-			int finalY2 = finalY1 + seed.Next(minHouseDimension, maxHouseDimension) - 1;
-
-			var doorDirection = Directions.All4.GetRandom(seed);
-
-			region.CreateWalls(seed, finalX1, finalX2, finalY1, finalY2, z, doorDirection);
-
-			new Fire().placeIntoFreeCell(region, seed, finalX1 + 1, finalX2 - 1, finalY1 + 1, finalY2 - 1, z);
-			new Bed().placeIntoFreeCell(region, seed, finalX1 + 1, finalX2 - 1, finalY1 + 1, finalY2 - 1, z);
-			new Bed().placeIntoFreeCell(region, seed, finalX1 + 1, finalX2 - 1, finalY1 + 1, finalY2 - 1, z);
-
-			var allHouseCells = new List<Cell>();
-			for (int x = finalX1; x <= finalX2; x++)
-			{
-				for (int y = finalY1; y <= finalY2; y++)
-				{
-					allHouseCells.Add(region.GetCell(x, y, z));
-				}
-			}
-
-			var house = new House(allHouseCells);
-			region.Places.Add(house);
-			return house;
-		}
-
 		public static Npc CreateFamily(this Region region, Balance balance, Random seed, Race race, Profession profession, string surname, int x1, int x2, int y1, int y2, int z, House house)
 		{
 			var hairColors = race.HairColors.ToList();
@@ -169,53 +217,6 @@ namespace Roguelike.Core
 			var transport = new Horse(balance, false, Time.FromYears(balance.Time, balance.Time.BeginYear).AddYears(-10), Color.LightSalmon);
 			transport.GetAspect<Ownership>().OwnBy(owner);
 			transport.placeIntoFreeCell(region, seed, x1, x2, y1, y2, z);
-		}
-
-		public static void CreateVillage(this Region region, Balance balance, Random seed, Language language, int x1, int x2, int y1, int y2, int z)
-		{
-			if (Math.Abs(x1 - x2) < 10 || Math.Abs(y1 - y2) < 10) throw new Exception("Village is too small - 10x10 is minimal size.");
-
-			const int minHouseDimension = 5;
-			const int maxHouseDimension = 7;
-
-			int treesCount = (x2 - x1) * (y2 - y1) / 10;
-			for (int i = 0; i < treesCount; i++)
-			{
-				new Tree().placeIntoFreeCell(region, seed, x1, x2, y1, y2, z);
-			}
-
-			int poolsCount = (x2 - x1) * (y2 - y1) / 30;
-			for (int i = 0; i < poolsCount; i++)
-			{
-				new Pool().placeIntoFreeCell(region, seed, x1, x2, y1, y2, z);
-			}
-
-			var houses = new List<House>();
-			int houseY1 = y1 + 1;
-			while (houseY1 + maxHouseDimension + 1 < y2)
-			{
-				int houseX1 = x1 + 1;
-				while (houseX1 + maxHouseDimension + 1 < x2)
-				{
-					houses.Add(region.CreateHouse(seed, minHouseDimension, maxHouseDimension, houseX1, houseY1, z));
-
-					houseX1 += maxHouseDimension + seed.Next(1, 2);
-				}
-
-				houseY1 += maxHouseDimension + seed.Next(1, 2);
-			}
-
-			for (int i = 0; i < houses.Count; i++)
-			{
-				var race = Race.PlainsMan;
-				var profession = Profession.Everyman;
-				string surname = profession.IsSurname
-					? profession.GetName(language.Character.Professions)
-					: race.Surnames[i % race.Surnames.Count];
-				var husband = region.CreateFamily(balance, seed, race, profession, surname, x1, x2, y1, y2, z, houses[i]);
-
-				region.CreateAnimals(balance, seed, husband, x1, x2, y1, y2, z);
-			}
 		}
 
 		private static void placeIntoFreeCell(this Object @object, Region region, Random seed, int x1, int x2, int y1, int y2, int z)

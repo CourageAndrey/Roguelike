fea8a71acb27b9ce92174e2ca197293130a6eebb
Andrei Bychko <andrei.a.bychko@gmail.com>
2025-11-19 15:58:50 +0100

    Center console screen.

--- Code/Roguelike.Console/ConsoleUi.System.cs
+++ Code/Roguelike.Console/ConsoleUi.System.cs
@@ -1,32 +1,90 @@
 ﻿using System;
 using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
 
 namespace Roguelike.Console
 {
 	partial class ConsoleUi
 	{
-		private static void LockConsoleSize(int width, int height)
+		[SupportedOSPlatform("windows")]
+		private static void AdjustWindowsConsole(int width, int height)
+		{
+			IntPtr handle = GetConsoleWindow();
+			if (handle != IntPtr.Zero)
+			{
+				LockConsoleSize(handle, width, height);
+				CenterConsoleWindow(handle);
+			}
+		}
+
+		[SupportedOSPlatform("windows")]
+		private static void LockConsoleSize(IntPtr handle, int width, int height)
 		{
 			System.Console.SetBufferSize(width, height);
 			System.Console.SetWindowSize(width, height);
 
-			IntPtr handle = GetConsoleWindow();
 			IntPtr sysMenu = GetSystemMenu(handle, false);
 			DeleteMenu(sysMenu, SC_MAXIMIZE, MF_BYCOMMAND);
 			DeleteMenu(sysMenu, SC_SIZE, MF_BYCOMMAND);
 		}
 
+		[SupportedOSPlatform("windows")]
+		private static void CenterConsoleWindow(IntPtr handle)
+		{
+			if (GetWindowRect(handle, out var rect))
+			{
+				int windowWidth = rect.Right - rect.Left;
+				int windowHeight = rect.Bottom - rect.Top;
+
+				int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+				int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+				int x = (screenWidth - windowWidth) / 2;
+				int y = (screenHeight - windowHeight) / 2;
+
+				SetWindowPos(handle, IntPtr.Zero, x, y, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+			}
+		}
+
+		[SupportedOSPlatform("windows")]
 		[DllImport("kernel32.dll", ExactSpelling = true)]
 		private static extern IntPtr GetConsoleWindow();
 
+		[SupportedOSPlatform("windows")]
 		[DllImport("user32.dll")]
 		private static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
 
+		[SupportedOSPlatform("windows")]
 		[DllImport("user32.dll")]
 		private static extern bool DeleteMenu(IntPtr hMenu, uint uPosition, uint uFlags);
+
+		[SupportedOSPlatform("windows")]
+		[DllImport("user32.dll")]
+		private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+		[SupportedOSPlatform("windows")]
+		[DllImport("user32.dll")]
+		private static extern int GetSystemMetrics(int nIndex);
+
+		[SupportedOSPlatform("windows")]
+		[DllImport("user32.dll", SetLastError = true)]
+		private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+		[StructLayout(LayoutKind.Sequential)]
+		private struct RECT
+		{
+			public int Left;
+			public int Top;
+			public int Right;
+			public int Bottom;
+		}
 
 		private const uint SC_MAXIMIZE = 0xF030;
 		private const uint SC_SIZE = 0xF000;
 		private const uint MF_BYCOMMAND = 0x00000000;
+		private const int SM_CXSCREEN = 0;
+		private const int SM_CYSCREEN = 1;
+		private const uint SWP_NOSIZE = 0x0001;
+		private const uint SWP_NOZORDER = 0x0004;
 	}
 }

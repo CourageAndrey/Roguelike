4ab6ea306b21dc98258b41a6c6c67aab4ee2ea50
Andrei Bychko <andrei.a.bychko@gmail.com>
2025-12-08 13:47:05 +0100

    Allow to highligt long console text.

--- Code/Roguelike.Core/Localization/Language.cs
+++ Code/Roguelike.Core/Localization/Language.cs
@@ -110,26 +110,26 @@ namespace Roguelike.Core.Localization
 				HelpTitle = "Help",
 				HelpText = @"Key bindings:
 
-F1 - show help
-F2 - show Character's menu
-F3 - show Character's equipment
-F4 - show Character's inventory
-F5 - show events log
+{F1} - show help
+{F2} - show Character's menu
+{F3} - show Character's equipment
+{F4} - show Character's inventory
+{F5} - show events log
 
-arrow keys & NumPad keys - move (or attack if aggressive)
+{arrow keys} & {NumPad keys} - move (or attack if aggressive)
 
-h - interact with object (sleep on bed, cook on fire, etc.)
-f - change aggressive state
-d - drop item
-w - select weapon
-c - chat
-t - shoot
-T - trade
-x - sneak
-r - read book
-o - open/close (door)
-s - ride (horse)
-, - pick item",
+{h} - interact with object (sleep on bed, cook on fire, etc.)
+{f} - change aggressive state
+{d} - drop item
+{w} - select weapon
+{c} - chat
+{t} - shoot
+{T} - trade
+{x} - sneak
+{r} - read book
+{o} - open/close (door)
+{s} - ride (horse)
+{,} - pick item",
 
 				Directions = LanguageDirections.CreateDefault(),
 

11f409a3edee1f8c73f7ee3cf5295332358a189d
Andrei Bychko <abychko@aras.com>
2025-12-22 10:41:35 +0100

    Just add DamageType and WeaponMastery lists.

---
+++ Code/Roguelike.Core/Mechanics/WeaponMastery.cs
@@ -0,0 +1,25 @@
+﻿namespace Roguelike.Core.Mechanics
+{
+	public class WeaponMastery
+	{
+		public bool IsRange
+		{ get; }
+
+		private WeaponMastery(bool isRange)
+		{
+			IsRange = isRange;
+		}
+
+		#region List
+
+		public static readonly WeaponMastery HandToHand = new WeaponMastery(false);
+		public static readonly WeaponMastery Blades = new WeaponMastery(false);
+		public static readonly WeaponMastery Axes = new WeaponMastery(false);
+		public static readonly WeaponMastery Maces = new WeaponMastery(false);
+		public static readonly WeaponMastery Polearms = new WeaponMastery(false);
+		public static readonly WeaponMastery Bows = new WeaponMastery(true);
+		public static readonly WeaponMastery Throwing = new WeaponMastery(true);
+
+		#endregion
+	}
+}
